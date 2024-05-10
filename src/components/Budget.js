7cccbaafc854ba7550a2b4af1c43d3bb8a86885d
Andrea Allegri <andrea.allegri92@gmail.com>
2024-05-10 11:23:15 -0400

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,24 @@
 
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const { dispatch } = useContext(AppContext);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        });
+    }
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £  </span>
+            <input type="number" step="50" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
+    );
+};
+export default Budget;
